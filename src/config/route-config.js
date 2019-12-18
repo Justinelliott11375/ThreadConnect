d0659ead4ca748b9b31ca92e62ea5ec5e09f09cc
Justin Elliott <Justinelliott11375@gmail.com>
2019-12-18 00:10:32 -0600

    minor bugs but fully functioning

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,6 +5,7 @@ module.exports = {
         const topicRoutes = require("../routes/topics");
         const userRoutes = require("../routes/users");
         const commentRoutes = require("../routes/comments");
+        const voteRoutes = require("../routes/votes");
 
         if (process.env.NODE_ENV === "test") {
             const mockAuth = require("../../spec/support/mock-auth.js");
@@ -15,5 +16,6 @@ module.exports = {
         app.use(topicRoutes);
         app.use(userRoutes);
         app.use(commentRoutes);
+        app.use(voteRoutes);
     }
 }
--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -2,6 +2,7 @@ const Post = require("./models").Post;
 const Topic = require("./models").Topic;
 const Comment = require("./models").Comment;
 const User = require("./models").User;
+const Vote = require("./models").Vote;
 const Authorizer = require("../policies/post");
 
 module.exports = {
@@ -18,12 +19,17 @@ module.exports = {
     getPost(id, callback) {
         return Post.findById(id, {
                 include: [{
-                    model: Comment,
-                    as: "comments",
-                    include: [{
-                        model: User
-                    }]
-                }]
+                        model: Comment,
+                        as: "comments",
+                        include: [{
+                            model: User
+                        }]
+                    },
+                    {
+                        model: Vote,
+                        as: "votes"
+                    }
+                ]
             })
             .then((post) => {
                 callback(null, post);
